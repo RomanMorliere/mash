46e890660d0da1c5ca8a80c5868376a29c2bf041
Roman Morliere <roman.morliere@epita.fr>
2026-03-05 12:45:04 +0100

    Initial

---
+++ script.js
@@ -0,0 +1,8 @@
+const navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+if (navToggle && navMenu) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+  });
+}
